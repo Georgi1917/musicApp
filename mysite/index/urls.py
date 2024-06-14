16d30e36dfb445377f533c77bc38f767d6f8fdaa
Gosho Zvqra <mamarida15@abv.bg>
2024-06-14 21:25:28 +0300

    first commit, user auth

---
+++ mysite/index/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from index.views import home_page_index, login_user, register_user, show_main_page, logout_user
+
+urlpatterns = [
+    path('', home_page_index, name="home"),
+    path('log-in-page', login_user, name="login"),
+    path('register-page', register_user, name="register"),
+    path('main-page', show_main_page, name="main-page"),
+    path('logout_user', logout_user, name="logout")
+]
