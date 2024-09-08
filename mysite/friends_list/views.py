82725e11f4c58a203726b729ceabcfc98c580811
Gosho Zvqra <mamarida15@abv.bg>
2024-09-08 20:05:19 +0300

    further work on the friends logic

--- mysite/friends_list/views.py
+++ mysite/friends_list/views.py
@@ -2,20 +2,43 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.urls import reverse
 from friends_list.models import FriendList
+from django.db.models import Q
 
 # Create your views here.
 
 def show_friends_list(request, user_id):
     searched_user = request.GET.get('searched_names', '')
+
     users = User.objects.filter(username__icontains=searched_user) if searched_user else []
-    sent_friend_requests = FriendList.objects.filter(sender_id=request.user.id)
-    received_friend_requests = FriendList.objects.filter(receiver_id=request.user.id)
+
+    sent_friend_requests = FriendList.objects.filter(
+        Q(sender_id=request.user.id) & Q(status="Pending")
+    )
+
+    received_friend_requests = FriendList.objects.filter(
+        Q(receiver_id=request.user.id) & Q(status="Pending")
+    )
+
+    accepted_friends = FriendList.objects.filter(
+        Q(sender_id=user_id) | Q(receiver_id=user_id),
+        Q(status="Accepted")
+    )
+
+    needed_users = [
+        x for x in users
+        if (
+        (x.pk not in list(map(lambda x: x.receiver_id, sent_friend_requests))) and
+        (x.pk not in list(map(lambda x: x.sender_id,received_friend_requests))) and
+        (x.pk != request.user.id) and
+        (x not in accepted_friends))
+    ]
 
     context = {
-        "users": users,
+        "users": needed_users,
         "searched_user": searched_user,
         "sent_friend_requests": sent_friend_requests,
-        "received_friend_requests": received_friend_requests
+        "received_friend_requests": received_friend_requests,
+        "friends": accepted_friends
     }
 
     return render(request, 'friends_list.html', context=context)
