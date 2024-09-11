aa5e74202cff3469ad8b4c2709154ad06e461c9b
Gosho Zvqra <mamarida15@abv.bg>
2024-09-11 16:43:47 +0300

    work on friends list

--- mysite/friends_list/models.py
+++ mysite/friends_list/models.py
@@ -3,7 +3,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class FriendList(models.Model):
+class FriendRequestList(models.Model):
     sender = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="sent_friend_request")
     receiver = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="received_friend_request")
     status = models.CharField(max_length=30)
+
+class FriendList(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="main_friend")
+    friends = models.ManyToManyField(to=User, related_name="all_friends")
--- mysite/friends_list/views.py
+++ mysite/friends_list/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.urls import reverse
-from friends_list.models import FriendList
+from friends_list.models import FriendRequestList
 from django.db.models import Q
 
 # Create your views here.
@@ -11,17 +11,12 @@ def show_friends_list(request, user_id):
 
     users = User.objects.filter(username__icontains=searched_user) if searched_user else []
 
-    sent_friend_requests = FriendList.objects.filter(
+    sent_friend_requests = FriendRequestList.objects.filter(
         Q(sender_id=request.user.id) & Q(status="Pending")
     )
 
-    received_friend_requests = FriendList.objects.filter(
+    received_friend_requests = FriendRequestList.objects.filter(
         Q(receiver_id=request.user.id) & Q(status="Pending")
-    )
-
-    accepted_friends = FriendList.objects.filter(
-        Q(sender_id=user_id) | Q(receiver_id=user_id),
-        Q(status="Accepted")
     )
 
     needed_users = [
@@ -29,8 +24,7 @@ def show_friends_list(request, user_id):
         if (
         (x.pk not in list(map(lambda x: x.receiver_id, sent_friend_requests))) and
         (x.pk not in list(map(lambda x: x.sender_id,received_friend_requests))) and
-        (x.pk != request.user.id) and
-        (x not in accepted_friends))
+        (x.pk != request.user.id))
     ]
 
     context = {
@@ -38,7 +32,6 @@ def show_friends_list(request, user_id):
         "searched_user": searched_user,
         "sent_friend_requests": sent_friend_requests,
         "received_friend_requests": received_friend_requests,
-        "friends": accepted_friends
     }
 
     return render(request, 'friends_list.html', context=context)
@@ -47,6 +40,9 @@ def send_friend_request(request, user_id, receiver_id):
     sender = get_object_or_404(User, id=user_id)
     receiver = get_object_or_404(User, id=receiver_id)
 
-    friend_request = FriendList.objects.create(sender=sender, receiver=receiver, status="Pending")
+    friend_request = FriendRequestList.objects.create(sender=sender, receiver=receiver, status="Pending")
 
     return redirect("friends-list", user_id=user_id)
+
+def accept_friend_request(request):
+    pass
