ee818b9f4ce1d805c99d30b6641585a96c9b6eda
Gosho Zvqra <mamarida15@abv.bg>
2024-09-13 22:07:09 +0300

    added the add friend function

--- mysite/friends_list/urls.py
+++ mysite/friends_list/urls.py
@@ -3,5 +3,6 @@ from friends_list import views
 
 urlpatterns = [
     path('friend-list', views.show_friends_list, name="friends-list"),
-    path('friends-list/<int:receiver_id>', views.send_friend_request, name="send-request")
+    path('friends-list/<int:receiver_id>', views.send_friend_request, name="send-request"),
+    path('accept-request/<int:sender_id>', views.accept_friend_request, name="accept-request")
 ]
--- mysite/friends_list/views.py
+++ mysite/friends_list/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.urls import reverse
-from friends_list.models import FriendRequestList
+from friends_list.models import FriendRequestList, FriendList
 from django.db.models import Q
 
 # Create your views here.
@@ -19,11 +19,14 @@ def show_friends_list(request, user_id):
         Q(receiver_id=request.user.id) & Q(status="Pending")
     )
 
+    friends_list = FriendList.objects.get(user_id=user_id).friends.all()
+
     needed_users = [
         x for x in users
         if (
         (x.pk not in list(map(lambda x: x.receiver_id, sent_friend_requests))) and
         (x.pk not in list(map(lambda x: x.sender_id,received_friend_requests))) and
+        (x not in friends_list) and
         (x.pk != request.user.id))
     ]
 
@@ -32,6 +35,7 @@ def show_friends_list(request, user_id):
         "searched_user": searched_user,
         "sent_friend_requests": sent_friend_requests,
         "received_friend_requests": received_friend_requests,
+        "friends_list": friends_list
     }
 
     return render(request, 'friends_list.html', context=context)
@@ -44,5 +48,45 @@ def send_friend_request(request, user_id, receiver_id):
 
     return redirect("friends-list", user_id=user_id)
 
-def accept_friend_request(request):
-    pass
+def accept_friend_request(request, user_id, sender_id):
+    sender_user = User.objects.get(pk=sender_id)
+    receiver_user = User.objects.get(pk=user_id)
+
+    friend_list_receiver = FriendList.objects.filter(user=receiver_user).first()
+
+    if not friend_list_receiver:
+        friend_list_receiver = FriendList.objects.create(user=receiver_user)
+        
+        if sender_user not in friend_list_receiver.friends.all():
+            friend_list_receiver.friends.add(sender_user)
+
+    else:
+
+        if sender_user not in friend_list_receiver.friends.all():
+            friend_list_receiver.friends.add(sender_user)
+
+    friend_list_sender = FriendList.objects.filter(user=sender_user).first()
+
+    if not friend_list_sender:
+        friend_list_sender = FriendList.objects.create(user=sender_user)
+
+        if receiver_user not in friend_list_sender.friends.all():
+            friend_list_sender.friends.add(receiver_user)
+
+    else:
+
+        if receiver_user not in friend_list_sender.friends.all():
+            friend_list_sender.friends.add(receiver_user)
+
+    friend_request = FriendRequestList.objects.get(
+        Q(receiver_id=user_id) & Q(sender_id=sender_id)
+    )
+    friend_request.status = "Accepted"
+    friend_request.save()
+
+    print(friend_list_receiver.friends.all())
+    print(friend_list_sender.friends.all())
+
+    return redirect("friends-list", user_id=user_id)
+
+    
