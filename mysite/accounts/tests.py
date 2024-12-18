cc2cc9d5f94e0f83354a03f19d2b5511c8a3e0cc
Gosho Zvqr <mamarida15@abv.bg>
2024-12-18 18:11:57 +0200

    feat: added tests for accounts and album_creation apps

--- mysite/accounts/tests.py
+++ mysite/accounts/tests.py
@@ -1,3 +1,94 @@
 from django.test import TestCase
+from accounts.models import AppUser
+from django.urls import reverse_lazy
 
-# Create your tests here.
+
+class TestUserCreation(TestCase):
+
+    def test_get_correct_template(self):
+
+        response = self.client.get(reverse_lazy("register"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "accounts/register.html")
+        self.assertContains(response, "form")
+        self.assertContains(response, "csrfmiddlewaretoken")
+
+    def test_correct_user_creation(self):
+
+        form_data = {
+            "email": "pesho@pesho.bg",
+            "username": "GeorgiGogo",
+            "password1": "12admin34",
+            "password2": "12admin34",
+        }
+
+        response = self.client.post(reverse_lazy("register"), data=form_data)
+
+        self.assertEqual(AppUser.objects.count(), 1)
+        self.assertEqual(AppUser.objects.first().username, "GeorgiGogo")
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy("main-page"))
+
+    def test_incorrect_user_creation(self):
+
+        form_data = {
+            "email": "",
+            "username": "Gosho",
+            "password1": "12admin34",
+            "password2": "12admin34",
+        }
+
+        response = self.client.post(reverse_lazy("register"), data=form_data)
+
+        self.assertEqual(AppUser.objects.count(), 0)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "accounts/register.html")
+        self.assertContains(response, "This field is required")
+
+
+class TestUserLogin(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        
+        cls.user = AppUser.objects.create_user(
+            email="pesho@pesho.com",
+            username="PeshoGotin",
+            password="12admin34",
+        )
+
+    def test_correct_login(self):
+
+        response = self.client.post(reverse_lazy("login"), {
+            "username": "PeshoGotin",
+            "password": "12admin34",
+        })
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy("main-page"))
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+
+    def test_incorrect_login(self):
+
+        response = self.client.post(reverse_lazy("login"), {
+            "username": "Wrong User",
+            "password": "Wrong User Password"
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Please enter a correct username and password.")
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
+
+    def test_blank_login(self):
+
+        response = self.client.post(reverse_lazy("login"), {
+            "username": "",
+            "password": "",
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "This field is required.")
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
--- mysite/album_creation/tests.py
+++ mysite/album_creation/tests.py
@@ -1,3 +1,164 @@
 from django.test import TestCase
+from album_creation.models import Playlist, FollowedPlaylist
+from accounts.models import AppUser
+from django.urls import reverse_lazy
 
-# Create your tests here.
+
+class TestPlaylistPage(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        
+        cls.user = AppUser.objects.create_user(
+            email="pesho@pesho.bg",
+            username="PeshoGotin",
+            password="12admin34",
+        )
+
+        cls.another_user = AppUser.objects.create_user(
+            email="ivan@ivanov.bg",
+            username="IvanIvanov",
+            password="12admin34",
+        )
+
+        cls.playlist1 = Playlist.objects.create(
+            name="Rock",
+            description="A cool rock playlist",
+            logo=None,
+            user=cls.user
+        )
+
+        cls.playlist2 = Playlist.objects.create(
+            name="Folk",
+            description="A cool folk playlist",
+            logo=None,
+            user=cls.user
+        )
+
+        cls.playlist3 = Playlist.objects.create(
+            name="Regae",
+            description="A cool regae playlist",
+            logo=None,
+            user=cls.another_user
+        )
+
+        cls.playlist4 = Playlist.objects.create(
+            name="Heavy Metal",
+            description="A cool Heavy Metal Playlist",
+            logo=None,
+            user=cls.another_user
+        )
+
+        cls.followed_playlist1 = FollowedPlaylist.objects.create(
+            user=cls.user,
+            playlist=cls.playlist3
+        )
+
+        cls.followed_playlist2 = FollowedPlaylist.objects.create(
+            user=cls.user,
+            playlist=cls.playlist4
+        )
+
+    def test_playlist_page_not_authenticated_user_status_code(self):
+
+        response = self.client.get(reverse_lazy("playlist-page"))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f"{reverse_lazy("login")}?next={reverse_lazy("playlist-page")}")
+
+    def test_playlist_page_not_authenticated_user_template_user(self):
+
+        response = self.client.get(reverse_lazy("playlist-page"))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateNotUsed(response, "album_creation/playlist.page")
+        self.assertRedirects(response, f"{reverse_lazy("login")}?next={reverse_lazy("playlist-page")}")
+
+    def test_playlist_page_authenticated_user_status_code(self):
+
+        self.client.login(username="PeshoGotin", password="12admin34")
+
+        response = self.client.get(reverse_lazy("playlist-page"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_playlist_page_authenticated_user_template(self):
+
+        self.client.login(username="PeshoGotin", password="12admin34")
+
+        response = self.client.get(reverse_lazy("playlist-page"))
+        self.assertTemplateUsed(response, "album_creation/playlist-page.html")
+
+    def test_playlist_page_context_data(self):
+
+        self.client.login(username="PeshoGotin", password="12admin34")
+
+        response = self.client.get(reverse_lazy("playlist-page"))
+        self.assertEqual(list(response.context["user_playlists"]), [self.playlist1, self.playlist2])
+        self.assertEqual(list(response.context["followed_playlists"]), [self.followed_playlist1, self.followed_playlist2])
+
+
+class TestEditPlaylistPage(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        
+        cls.user = AppUser.objects.create_user(
+            email="pesho@pesho.bg",
+            username="PeshoGotin",
+            password="12admin34",
+        )
+
+        cls.playlist = Playlist.objects.create(
+            name="Rock",
+            description="A cool Rock Playlist",
+            logo=None,
+            user=cls.user
+        )
+
+    def setUp(self):
+        
+        self.client.login(username="PeshoGotin", password="12admin34")
+
+    def test_edit_playlist_page_status_code(self):
+
+        response = self.client.get(reverse_lazy("edit-playlist", kwargs={"album_id": self.playlist.id}))
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_playlist_page_template_used(self):
+
+        response = self.client.get(reverse_lazy("edit-playlist", kwargs={"album_id": self.playlist.id}))
+
+        self.assertTemplateUsed(response, "album_creation/edit_album.html")
+
+    def test_edit_playlist_page_updates_playlist_on_post(self):
+
+        post_data = {
+            "name": "Folk",
+            "description": "I Love Folk Music!",
+        }
+
+        response = self.client.post(reverse_lazy("edit-playlist", kwargs={"album_id": self.playlist.id}), data=post_data)
+
+        self.playlist.refresh_from_db()
+
+        self.assertEqual(self.playlist.name, "Folk")
+        self.assertEqual(self.playlist.description, "I Love Folk Music!")
+
+        self.assertRedirects(response, reverse_lazy("playlist-page"))
+
+    def test_edit_playlist_page_404_on_invalid_id(self):
+
+        response = self.client.get(reverse_lazy("edit-playlist", kwargs={"album_id": 100000}))
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_edit_playlist_page_redirects_on_non_logged_in_users(self):
+
+        self.client.logout()
+
+        response = self.client.get(reverse_lazy("edit-playlist", kwargs={"album_id": self.playlist.id}))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateNotUsed(response, "album_creation/edit_album.html")
+        self.assertRedirects(response, f"{reverse_lazy("login")}?next={reverse_lazy("edit-playlist", kwargs={"album_id": self.playlist.id})}")
