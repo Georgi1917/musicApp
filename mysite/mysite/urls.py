086ef46784166cb8ed05c477f7dc6155afdcbdcd
Gosho Zvqr <mamarida15@abv.bg>
2024-12-19 18:19:05 +0200

    before collecting static

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -31,6 +31,5 @@ urlpatterns = [
     path('', include('friends_list.urls'))
 ]
 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
